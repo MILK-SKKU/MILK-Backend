53fa3b8b88e5636bbade265a1668eb7699f2776f
ctwc55 <ssy990408@gmail.com>
2021-12-14 21:16:12 +0900

    add make_problem_with_solution function

--- problem.py
+++ problem.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-
+import json
 
 def get_problem():
     prob = {}
@@ -34,3 +34,23 @@ def create_problem(problem):
                 }
             ]
     })
+
+def make_problem_with_solution():
+    new_problem = {}
+    
+    with open("json/kdict_dummy.json", "r") as kdict_json:
+        kdict_data = json.load(kdict_json)
+
+    with open("json/problem_dummy.json", "r") as problem_json:
+        problem_data = json.load(problem_json) 
+
+    new_problem["context"] = problem_data["problems"][0]["context"]
+    new_problem["option"] = []
+    
+    for i in range(4): 
+        new_problem["option"].append(kdict_data["data"][i])
+        del new_problem["option"][i]["example"]
+    
+    new_problem["solution"] = 1
+
+    return jsonify(new_problem)
